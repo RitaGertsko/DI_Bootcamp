7442caca9046ea3ad7e5747c1a7abaaa4f352b38
Rita Margalit Gertskovich <rita1234rita@gmail.com>
2022-01-10 19:54:13 +0200

    adding js  exercises

---
+++ w3/day2/daily_challenge/main.js
@@ -0,0 +1,33 @@
+
+//Exercise 1:
+//1.
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits)
+
+//2.
+fruits = ["Apples", "Blueberries", "Oranges"];
+console.log(fruits)
+
+//3.
+fruits.push ("Kiwi");
+console.log(fruits)
+
+//4.
+fruits.splice (0,1);
+console.log(fruits)
+
+//5.
+fruits = ["Kiwi","Oranges","Blueberries"];
+console.log(fruits)
+
+
+
+//Exercise 2:
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let orange = moreFruits[1][1];
+console.log(orange)
+
+
+
+
